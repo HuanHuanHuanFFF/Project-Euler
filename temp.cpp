ba46058ff0e9060bcfa326d5fab5dc6dbd1525ec
HuanHuanHuanFFF <2445657276@qq.com>
2025-07-16 02:47:37 +0800

    模板

---
+++ temp.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::pair;
+using std::vector;
+using std::string;
+
+using ll = long long;
+using ull = unsigned long long;
+constexpr int MOD = 1e9 + 7;
+constexpr int INF = 1e9 + 17;
+constexpr int N = 1e7 + 7;
+
+signed main() {
+    return 0;
+}
