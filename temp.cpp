90247859a31c7dc172648d0167425833688f42e1
HuanHuanHuanFFF <2445657276@qq.com>
2025-07-18 18:05:46 +0800

    模板更新

--- temp.cpp
+++ temp.cpp
@@ -12,6 +12,14 @@ constexpr int MOD = 1e9 + 7;
 constexpr int INF = 1e9 + 17;
 constexpr int N = 1e7 + 7;
 
+void bf() {
+}
+
+void solve() {
+}
+
 signed main() {
+    bf();
+    solve();
     return 0;
 }
