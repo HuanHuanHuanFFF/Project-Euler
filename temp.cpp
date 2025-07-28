b8362aa6429dfc00e6ef15fd72d2997917c752a5
HuanHuanHuanFFF <2445657276@qq.com>
2025-07-28 16:01:27 +0800

    模板更新

--- temp.cpp
+++ temp.cpp
@@ -10,7 +10,15 @@ using ll = long long;
 using ull = unsigned long long;
 constexpr int MOD = 1e9 + 7;
 constexpr int INF = 1e9 + 17;
-constexpr int N = 1e7 + 7;
+constexpr int N = 1e5;
+
+auto get_now() {
+    return std::chrono::high_resolution_clock::now();
+}
+
+auto ms_dis(auto t2, auto t1) {
+    return std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count();
+}
 
 void bf() {
 }
@@ -19,7 +27,12 @@ void solve() {
 }
 
 signed main() {
+    auto t1 = get_now();
     bf();
+    auto t2 = get_now();
+    cout << ms_dis(t2, t1) << "ms\n";
     solve();
+    auto t3 = get_now();
+    cout << ms_dis(t3, t2) << "ms\n";
     return 0;
 }
