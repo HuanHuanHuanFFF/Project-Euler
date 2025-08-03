79963f9bcbe682733bdec960aaea0022e5d5c999
HuanHuanHuanFFF <2445657276@qq.com>
2025-08-04 01:44:16 +0800

    模板更新

--- temp.cpp
+++ temp.cpp
@@ -8,6 +8,8 @@ using std::string;
 
 using ll = long long;
 using ull = unsigned long long;
+using ld = long double;
+using PII = pair<int, int>;
 constexpr int MOD = 1e9 + 7;
 constexpr int INF = 1e9 + 17;
 constexpr int N = 1e5;
